2401da6ff6fa80c5b9e311f78a492fcd3edf096c
archeryue <archeryue7@gmail.com>
2022-10-03 22:24:44 +0800

    FIX: TcpServer socket close

--- net.go
+++ net.go
@@ -10,7 +10,6 @@ const BACKLOG int = 64
 
 func TcpServer(port int) (int, error) {
 	s, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-	defer unix.Close(s)
 	if err != nil {
 		log.Printf("init socket err: %v\n", err)
 		return -1, nil
@@ -18,19 +17,23 @@ func TcpServer(port int) (int, error) {
 	err = unix.SetsockoptByte(s, unix.SOL_SOCKET, unix.SO_REUSEADDR, 0x01)
 	if err != nil {
 		log.Printf("set SO_REUSEADDR err: %v\n", err)
+		unix.Close(s)
 		return -1, nil
 	}
 	var addr unix.SockaddrInet4
+	// golang.syscall will handle htons
 	addr.Port = port
-	//set addr.Addr = any(0)
+	// golang will set addr.Addr = any(0)
 	err = unix.Bind(s, addr)
 	if err != nil {
 		log.Printf("bind addr err: %v\n", err)
+		unix.Close(s)
 		return -1, nil
 	}
 	err = unix.Listen(s, BACKLOG)
 	if err != nil {
 		log.Printf("listen socket err: %v\n", err)
+		unix.Close(s)
 		return -1, nil
 	}
 	return s, nil
