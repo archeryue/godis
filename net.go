1b0be6d89da88f2d4234d8254325c7a18a182d67
archeryue <archeryue7@gmail.com>
2022-10-03 19:41:40 +0800

    MOD: refine TCPServer

--- net.go
+++ net.go
@@ -6,6 +6,8 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+const BACKLOG int = 64
+
 func TcpServer(port int) (int, error) {
 	s, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
 	defer unix.Close(s)
@@ -20,13 +22,13 @@ func TcpServer(port int) (int, error) {
 	}
 	var addr unix.SockaddrInet4
 	addr.Port = port
-	//TODO: set addr.Addr = any
+	//set addr.Addr = any(0)
 	err = unix.Bind(s, addr)
 	if err != nil {
 		log.Printf("bind addr err: %v\n", err)
 		return -1, nil
 	}
-	err = unix.Listen(s, 64)
+	err = unix.Listen(s, BACKLOG)
 	if err != nil {
 		log.Printf("listen socket err: %v\n", err)
 		return -1, nil
